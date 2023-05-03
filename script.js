9563eee76b3918ad89416249b8ac55fc4818927b
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-05-03 10:41:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function insert_Row() {
     //Write your code here
   var table = document.getElementById('sampleTable');
-	var row=document.createElement('tr');
-	var col1=document.createElement('td');
-	var col2=document.createElement('td');
-	col1.innerText='New Cell1';
-	col2.innerText='New Cell2';
-	row.appendChild(col1);
-	row.appendChild(col2);
-	table.prepend(row);
+
+	var newRow = table.insertRow(0);
+
+	var leftCell = newRow.insertCell(0);
+	var rightCell = newRow.insertCell(1);
+
+	leftCell.innerHTML = 'New Cell1';
+	rightCell.innerHTML = 'New Cell2';
+
 }
